48a433e7df4e9069434c24322254be21c84228e9
jamesbwn <raxioma@outlook.com>
2022-10-13 08:02:27 +1000

    complete api

--- apiIndex.js
+++ apiIndex.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const ethers = require('ethers');
 const gasABI = require('./gas.json')
 
-const WSS = "https://goerli.optimism.io/"
+const WSS = "https://goerli.infura.io/v3/ee6cfbccf40e427c9c1062066cb3a7b6"
 var provider = new ethers.providers.JsonRpcProvider(WSS);
 
 const gasAddress = '0x1896b6eC0D75198803Fc9877124C25D82f8f4A4b';
@@ -30,12 +30,16 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.get('/estimateGas', async function (req, res) {
 	const key = req.query.gas;
-	var elements
-	for (let i = 0; i < key.length; i++) {
-		elements.push(key[i]);
+	const obKeys = JSON.parse(key)
+	var elements = []
+	for (let i = 0; i < obKeys.length; i++) {
+		elements.push(obKeys[i]);
 	}
 	gasContract.storeGas(elements);
-
+	console.log('debug end')
 	res.send("success")
 })
 
+app.get('/', async function (req, res) {
+	res.send("test success")
+})
